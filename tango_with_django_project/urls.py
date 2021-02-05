7a47ab35a836ee199b0308cf68880ca964fe1a91
“Tariq <2482879a@student.gla.ac.uk>
2021-02-05 22:10:45 +0300

    second commit (3.5)

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
@@ -26,5 +28,6 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     # maps URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
+
